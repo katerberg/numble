4a0da2474d02fab5fdd068cd282f60e7eb2edef6
Mark Katerberg <katerberg@fastmail.fm>
2016-05-29 19:56:31 +0200

    Adds a failure condition if game does not load.

--- app/js/resultsCtrl.test.js
+++ app/js/resultsCtrl.test.js
@@ -1,17 +1,23 @@
 describe('ResultsCtrl', function() {
 
   var instance,
+    $rootScope,
     $scope,
+    $q,
     $location,
     stateService,
+    storageService,
     $controller;
 
   beforeEach(module('numbleApp'));
 
-  beforeEach(inject(function(_$controller_, _$rootScope_, _$location_, _stateService_) {
+  beforeEach(inject(function(_$controller_, _$rootScope_, _$location_, _stateService_, _storageService_, _$q_) {
+    $q = _$q_;
     $controller = _$controller_;
     $location = _$location_;
     stateService = _stateService_;
+    storageService = _storageService_;
+    $rootScope = _$rootScope_;
     $scope = _$rootScope_.$new();
   }));
 
@@ -44,6 +50,35 @@ describe('ResultsCtrl', function() {
         $scope.startOver();
 
         expect(stateService.reset).toHaveBeenCalled();
+      });
+    });
+
+    describe('#getShare()', function() {
+      beforeEach(function() {
+        spyOn(storageService, 'storeScore');
+      });
+
+      it('exposes storage promise', function() {
+        var expected = $q.defer().promise;
+        storageService.storeScore.and.returnValue(expected);
+
+        $scope.getShare();
+
+        expect($scope.storage).toBe(expected);
+      });
+
+      it('exposes storage location after resolving', function() {
+        var deferred = $q.defer(),
+          expected = Math.random();
+        storageService.storeScore.and.returnValue(deferred.promise);
+
+        $scope.getShare();
+        expect($scope.shareId).toBeUndefined();
+
+        deferred.resolve({name: expected});
+        $rootScope.$apply();
+
+        expect($scope.shareId).toBe(expected);
       });
     });
   });
